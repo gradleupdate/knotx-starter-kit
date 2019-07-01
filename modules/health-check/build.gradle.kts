854d6403db4898cb1f973add53c3402bd98431b8
Tomasz Michalak <tomek.michalak@gmail.com>
2019-07-01 15:54:24 +0200

    Fixing Dev guide, moving Knot.x version to gradle.properties

--- modules/health-check/build.gradle.kts
+++ modules/health-check/build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
 }
 
 dependencies {
-    implementation(platform("io.knotx:knotx-dependencies:${Build.KNOTX_VERSION}"))
+    implementation(platform("io.knotx:knotx-dependencies:${project.property("knotx.version")}"))
     "io.knotx:knotx".let { v ->
-        implementation("$v-fragments-handler-api:${Build.KNOTX_VERSION}")
+        implementation("$v-fragments-handler-api:${project.property("knotx.version")}")
     }
     "io.vertx:vertx".let { v ->
         implementation("$v-core")
