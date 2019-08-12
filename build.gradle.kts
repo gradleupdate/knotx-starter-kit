c82e4241a7f141c6e6318c820186b100c4a04390
Piotr Marcinkowski <piotr.marcinkowski@cognifide.com>
2019-08-12 11:40:51 +0200

    docker #2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
+import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
+import com.bmuschko.gradle.docker.tasks.container.DockerStopContainer
+import com.bmuschko.gradle.docker.tasks.container.extras.DockerWaitHealthyContainer
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+import com.bmuschko.gradle.docker.tasks.image.DockerRemoveImage
+
+
 plugins {
     id("io.knotx.distribution")  version "0.1.2"
-    id("com.bmuschko.docker-remote-api") version "4.9.0"
+    id("com.bmuschko.docker-remote-api") version "4.10.0"
     id("java")
 }
 
@@ -26,6 +34,82 @@ dependencies {
 sourceSets.named("test") {
     java.srcDir("functional/src/test/java")
 }
+
+tasks.register("prepareDocker") {
+    dependsOn("cleanDistribution", "assembleBaseDistribution", "copyDockerfile")
+}
+
+val dockerImageRef = "$buildDir/.docker/buildImage-imageId.txt"
+
+
+tasks.register<Copy>("copyDockerfile") {
+    group = "docker"
+
+    from("docker")
+    into("$buildDir")
+    expand("knotx_version" to project.property("knotx.version"))
+
+    mustRunAfter("cleanDistribution")
+}
+
+tasks.create("removeImage", DockerRemoveImage::class) {
+    group = "docker"
+
+    val spec = object : Spec<Task> {
+        override fun isSatisfiedBy(task: Task): Boolean {
+            return File(dockerImageRef).exists()
+        }
+    }
+    onlyIf(spec)
+
+    targetImageId(if (File(dockerImageRef).exists()) File(dockerImageRef).readText() else "")
+    onError {
+        if (!this.message!!.contains("No such image"))
+            throw this
+    }
+}
+
+val buildImage by tasks.creating(DockerBuildImage::class) {
+    group = "docker"
+    inputDir.set(file("$buildDir"))
+    tags.add("${project.property("docker.image.name")}:latest")
+    dependsOn("removeImage", "prepareDocker")
+}
+
+val createContainer by tasks.creating(DockerCreateContainer::class) {
+    group = "docker-functional-tests"
+    dependsOn(buildImage)
+    targetImageId(buildImage.getImageId())
+    portBindings.set(listOf("8092:8092"))
+    exposePorts("tcp", listOf(8092))
+    autoRemove.set(true)
+}
+
+val startContainer by tasks.creating(DockerStartContainer::class) {
+    group = "docker-functional-tests"
+    dependsOn(createContainer)
+    targetContainerId(createContainer.containerId)
+}
+
+val waitContainer by tasks.creating(DockerWaitHealthyContainer::class) {
+    group = "docker-functional-tests"
+    dependsOn(startContainer)
+    targetContainerId(createContainer.containerId)
+}
+
+val stopContainer by tasks.creating(DockerStopContainer::class) {
+    group = "docker-functional-tests"
+    targetContainerId(createContainer.containerId)
+}
+
+tasks.create("runTest", Test::class) {
+    group = "docker-functional-tests"
+    dependsOn(waitContainer)
+    finalizedBy(stopContainer)
+
+    include("**/*ITCase*")
+}
+
 
 allprojects {
     group = "com.project"
@@ -45,5 +129,5 @@ tasks.named("build") {
 
 //apply(from = "gradle/distribution.gradle.kts")
 apply(from = "gradle/javaAndUnitTests.gradle.kts")
-apply(from = "gradle/docker.gradle.kts")
+//apply(from = "gradle/docker.gradle.kts")
 
